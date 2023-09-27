e95932c1cd333ab820a9dd7cc2f9a5a977b12156
najwamhfuzah <146162831+najwamhfuzah@users.noreply.github.com>
2023-09-27 12:56:57 +0700

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedataa;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LuasSegitiga {
+    public static void main(String[] args) {
+   
+        double alas = 9;
+        double tinggi = 6;
+        double luas = (alas*tinggi)/2;
+        System.out.println("hasil: " + luas);
+    }
+}
